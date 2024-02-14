73ad152fc928add3382c9b52255203676d6c0ef8
Victor Brigido <victorbrigido@icloud.com>
2024-02-14 17:47:08 -0300

    Initial Commit

---
+++ MyBeer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBeer
+//
+//  Created by Victor Brigido on 14/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBeer/MyBeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBeerApp.swift
+//  MyBeer
+//
+//  Created by Victor Brigido on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
